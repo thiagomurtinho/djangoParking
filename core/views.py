e7a18939acf44571066237370e00c33271502376
thiagomurtinho <thiago.murtinho@live.com>
2019-07-27 10:46:55 -0300

    Feature: Create templates system

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context ={
+        'messenger': 'Hello word',
+    }
+
+    return render(request, 'core/index.html', context)
