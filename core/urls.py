995e5b0f45d0d3c44e21ea62de8475596ee196f8
thiagomurtinho <thiago.murtinho@live.com>
2019-07-27 11:32:54 -0300

    Feature: vehicles and rotary motion list template

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import home, personList
+from .views import (
+    home, 
+    personList,  
+    vehicleList,
+    rotaryMotion,
+    )
 
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('person_list/', personList, name='core_persons_list'),
+    path('vehicle_list/', vehicleList, name='core_vehicles_list'),
+    path('rotaryMotion_list/', rotaryMotion, name='core_rotaryMotion_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .models import Person
+from .models import (
+    Person,
+    Vehicle,
+    RotaryMotion
+    )
+
 
 def home(request):
     context ={
@@ -7,6 +12,7 @@ def home(request):
     }
 
     return render(request, 'core/index.html', context)
+
 
 def personList(request):
     people  = Person.objects.all()
@@ -16,3 +22,24 @@ def personList(request):
     }
 
     return render(request, 'core/personList.html', context)
+
+
+def vehicleList(request):
+    vehicles  = Vehicle.objects.all()
+    
+    context = {
+        'vehicles': vehicles,
+    }
+
+    return render(request, 'core/vehicleList.html', context)
+
+
+def rotaryMotion(request):
+    rotaryMotions  = RotaryMotion.objects.all()
+    
+    context = {
+        'rotaryMotions': rotaryMotions,
+    }
+
+    return render(request, 'core/rotaryMotionList.html', context)
+
